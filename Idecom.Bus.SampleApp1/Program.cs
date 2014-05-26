1ccb60f1ec431b989ba9fc06ce4871332834ea74
evgenyk <evgeny@komarevtsev.net>
2014-05-26 22:43:41 +1000

    code cleanup

--- Idecom.Bus.SampleApp1/Program.cs
+++ Idecom.Bus.SampleApp1/Program.cs
@@ -24,15 +24,6 @@ namespace Idecom.Bus.SampleApp1
                                        .Start();
 
 
-//            bus1.SendLocal(new SayHelloCommand("Hello local 1"));
-//            bus1.Send(new SayHelloCommand("Hello local 2"));
-//                        for (var i = 0; i < 1000000; i++)
-//            {
-//                PerfMetric.Received++;
-//                bus1.Send(new SayHelloCommand("Hello local: " + DateTime.Now));
-//            }
-
-
             Console.WriteLine("Bus configured. Press any key to close the app.");
             Console.ReadKey();
             busInstance.Stop();
