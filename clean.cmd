9181c96bf41f121a3d92371c0c836227c97d15b1
Evgeny Komarevtsev <evgeny@komarevtsev.net>
2014-06-27 14:52:45 +1000

    a new version to update package usages

---
+++ clean.cmd
@@ -0,0 +1,13 @@
+@ECHO OFF
+iisreset -stop
+
+%Windir%\System32\WindowsPowerShell\v1.0\Powershell.exe write-host -foregroundcolor Green Cleaning up Debug build...
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild Idecom.Bus.sln /v:q /p:VisualStudioVersion=12.0 /m  /target:Clean /p:Configuration=Debug /nologo
+%Windir%\System32\WindowsPowerShell\v1.0\Powershell.exe write-host -foregroundcolor Green Cleaning up Release build...
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild Idecom.Bus.sln /v:q /p:VisualStudioVersion=12.0 /m  /target:Clean /p:Configuration=Release /nologo
+%Windir%\System32\WindowsPowerShell\v1.0\Powershell.exe write-host -foregroundcolor Green Deleting packages folder...
+rd packages /S /Q
+
+iisreset -start
+
+%Windir%\System32\WindowsPowerShell\v1.0\Powershell.exe write-host -foregroundcolor Green Done.
