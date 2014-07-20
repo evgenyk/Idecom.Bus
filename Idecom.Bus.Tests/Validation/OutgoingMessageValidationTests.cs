2fa3e4be560b679438373cb5d98636634f960441
evgenyk <evgeny@komarevtsev.net>
2014-07-20 23:50:34 +1000

    started working on message validation

---
+++ Idecom.Bus.Tests/Validation/OutgoingMessageValidationTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Idecom.Bus.Tests.Validation
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using Implementations;
+    using InMemoryInfrastructure;
+    using IoC.CastleWindsor;
+    using Serializer.JsonNet;
+    using Xunit;
+
+    public class OutgoingMessageValidationTests
+    {
+        [Fact]
+        public void CanValidateWhileSendingAMessageTest()
+        {
+            var bus = Configure.With()
+                               .WindsorContainer()
+                               .InMemoryTransport()
+                               .InMemoryPubSub()
+                               .JsonNetSerializer()
+                               .CreateBus("app1")
+                               .Start();
+
+            Assert.Throws<Exception>(() => bus.SendLocal(new Tests.ACommand()));
+        }
+
+        public class ACommand
+        {
+            [Required]
+            public string InvalidProperty { get; set; }
+        }
+
+        public interface IEvent
+        {
+        }
+
+    }
+}
