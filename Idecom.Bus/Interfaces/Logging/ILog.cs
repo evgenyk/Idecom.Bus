6195e58f28d3d15abf131c6f5500db106728a2f9
evgenyk <evgeny@komarevtsev.net>
2014-10-05 23:58:04 +1100

    added interface for log

---
+++ Idecom.Bus/Interfaces/Logging/ILog.cs
@@ -0,0 +1,19 @@
+﻿namespace Idecom.Bus.Interfaces.Logging
+{
+    using System;
+
+    public interface ILog
+    {
+        void Debug(string message);
+        void Debug(string message, Exception exception);
+        void DebugFormat(string format, params object[] args);
+
+        void Info(string message);
+        void Info(string message, Exception exception);
+        void InfoFormat(string format, params object[] args);
+
+        void Error(string message);
+        void Error(string message, Exception exception);
+        void ErrorFormat(string format, params object[] args);
+    }
+}
