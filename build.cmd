cec17f3d8f1276abdc4817027c1105a5266662eb
evgenyk <evgeny@komarevtsev.net>
2014-10-15 22:30:32 +1100

    removed nuget restore from project files

--- build.cmd
+++ build.cmd
@@ -1,2 +1,3 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /property:Configuration=Debug /property:Platform="Any CPU" /m /v:m
+.nuget\nuget restore
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /property:Configuration=Debug /property:Platform="Any CPU" /m /v:m /nologo
 packages\xunit.runners.1.9.2\tools\xunit.console.clr4.x86.exe Idecom.Bus.Tests\bin\Debug\Idecom.Bus.Tests.dll
