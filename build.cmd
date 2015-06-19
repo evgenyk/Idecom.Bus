485621fcea900314b19ec9b6157d9ffdbdd4f517
evgeny.komarevtsev <evgeny@komarevtsev.net>
2015-06-19 13:36:23 +0300

    latest msbuild and compiler

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 .nuget\nuget restore
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /property:Configuration=Debug /property:Platform="Any CPU" /m /v:m /nologo
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /property:Configuration=Debug /property:Platform="Any CPU" /m /v:q /nologo 
 packages\xunit.runners.1.9.2\tools\xunit.console.clr4.x86.exe Idecom.Bus.Tests\bin\Debug\Idecom.Bus.Tests.dll
